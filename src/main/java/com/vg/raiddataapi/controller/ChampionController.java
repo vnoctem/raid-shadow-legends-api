58eecc3c1036409a98d82eebcd8c1204506bf08e
Vincent <vincent.ngo.gauthier@gmail.com>
2021-12-15 23:17:44 -0500

    Add API documentation with Swagger UI

--- src/main/java/com/vg/raiddataapi/controller/ChampionController.java
+++ src/main/java/com/vg/raiddataapi/controller/ChampionController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/champions")
+@RequestMapping("/v1/champions")
 public class ChampionController {
 
     @Autowired
--- src/main/java/com/vg/raiddataapi/controller/SkillController.java
+++ src/main/java/com/vg/raiddataapi/controller/SkillController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/skills")
+@RequestMapping("/v1/skills")
 public class SkillController {
 
     @Autowired
